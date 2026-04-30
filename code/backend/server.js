be632435a5203e6a7d8504635fa8464f88b78f85
tharakadenuwan <e22232@eng.pdn.ac.lk>
2026-04-30 20:23:16 +0530

    update

--- code/backend/server.js
+++ code/backend/server.js
@@ -10,7 +10,8 @@ const adminRouter = require('./routers/adminRouter');
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 const uri = 'mongodb://tharakadenuwan5555:tharaka@ac-z2gc85q-shard-00-00.4gvkuaf.mongodb.net:27017,ac-z2gc85q-shard-00-01.4gvkuaf.mongodb.net:27017,ac-z2gc85q-shard-00-02.4gvkuaf.mongodb.net:27017/test?ssl=true&replicaSet=atlas-8sd9oz-shard-0&authSource=admin&retryWrites=true&w=majority';
