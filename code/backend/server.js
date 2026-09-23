39c6751e3336c008d533066813dfc847d55d16c8
tharakadenuwan <e22232@eng.pdn.ac.lk>
2026-09-23 10:26:32 +0530

    update

--- code/backend/server.js
+++ code/backend/server.js
@@ -43,7 +43,7 @@ const connect = async () => {
 connect();
 
 const PORT = process.env.PORT || 3001;
-const server = app.listen(PORT, '127.0.0.1', () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
